f31f026efc62070e0df38fd0e316fb3c54b5f668
ferbarcante <barcantef@gmail.com>
2024-09-28 13:29:54 -0300

    first commit

---
+++ resources/views/user.blade.php
@@ -0,0 +1,14 @@
+<h1>Hello World</h1>
+ <p>{{$name}} is {{$age}}</p> 
+ {{$user->name}} <br>
+ {{$user->email}} <br>
+
+ <br>
+ {{date('d/m/y')}}
+ <br>
+
+ @if($user->name === 'Drake Keebler')
+    {{$user->name}}
+@elseif($user->name === 'Shane Wehner')
+    {{$user->name}}
+ @endif
